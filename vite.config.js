d3bcfe19e92fade1c8e7ea6e747f131ac94d1468
AlexDev404 <34188635+AlexDev404@users.noreply.github.com>
2025-09-21 21:04:10 -0600

    Set the app's env dir, and rename the app

--- vite.config.js
+++ vite.config.js
@@ -1,44 +1,41 @@
-
 import path from 'path';
 
-
-
 const SRC_DIR = path.resolve(__dirname, './src');
+const ENV_DIR = path.resolve(__dirname, './');
 const PUBLIC_DIR = path.resolve(__dirname, './public');
-const BUILD_DIR = path.resolve(__dirname, './www',);
+const BUILD_DIR = path.resolve(__dirname, './www');
 export default async () => {
-  const { svelte } = await import('@sveltejs/vite-plugin-svelte');
-  return  {
-    plugins: [
-      svelte({
-        // compilerOptions: {
-        //   compatibility: {
-        //     componentApi: 4
-        //   }
-        // }
-      }),
-
-    ],
-    root: SRC_DIR,
-    base: '',
-    publicDir: PUBLIC_DIR,
-    build: {
-      outDir: BUILD_DIR,
-      assetsInlineLimit: 0,
-      emptyOutDir: true,
-      rollupOptions: {
-        treeshake: false,
-      },
-    },
-    resolve: {
-      alias: {
-        '@': SRC_DIR,
-        '$root': SRC_DIR,
-      },
-    },
-    server: {
-      host: true,
-    },
-
-  };
-}
+	const { svelte } = await import('@sveltejs/vite-plugin-svelte');
+	return {
+		plugins: [
+			svelte({
+				// compilerOptions: {
+				//   compatibility: {
+				//     componentApi: 4
+				//   }
+				// }
+			})
+		],
+		root: SRC_DIR,
+		base: '',
+		publicDir: PUBLIC_DIR,
+		build: {
+			outDir: BUILD_DIR,
+			assetsInlineLimit: 0,
+			emptyOutDir: true,
+			rollupOptions: {
+				treeshake: false
+			}
+		},
+		envDir: ENV_DIR,
+		resolve: {
+			alias: {
+				'@': SRC_DIR,
+				$root: SRC_DIR
+			}
+		},
+		server: {
+			host: true
+		}
+	};
+};
